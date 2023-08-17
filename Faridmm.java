33d062f9833769d144d6427d1c412da0e727645b
alexefrain17 <1432461@senati.pe>
2023-08-17 10:24:32 -0500

    cree una calculadora gaa

--- Faridmm.java
+++ Faridmm.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.faridmm;
-
+import java.util.Scanner;
 /**
  *
  * @author Alumno
@@ -11,6 +11,70 @@ package com.mycompany.faridmm;
 public class Faridmm {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    System.out.println("Hello World!");
+        System.out.println(); 
+        //SUMAAAAAAA
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("La Suma");
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        
+        int numero1 = scanner.nextInt();
+        
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero2 = scanner.nextInt();
+        
+        int suma = numero1 + numero2;
+            System.out.println("la suma de " + numero1 + " + " + numero2 + " es: " + suma);
+                
+            
+        System.out.println();     
+        //RESTAAAAAA    
+        System.out.println("La Resta");    
+            
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        
+        int numero3 = scanner.nextInt();
+        
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero4 = scanner.nextInt();
+        
+        int resta = numero3 - numero4;
+            System.out.println("la resta de " + numero3 + " - " + numero4 + " es: " + resta);    
+            
+        System.out.println();    
+        //DIVISIOOOON
+        System.out.println("La Division");    
+            
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        
+        int numero5 = scanner.nextInt();
+        
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero6 = scanner.nextInt();
+        
+        int division = numero5 / numero6;
+            System.out.println("la resta de " + numero5 + " / " + numero6 + " es: " + division);
+        
+            
+        System.out.println();     
+        //MULTIPLICACIOOOON
+        System.out.println("La Multiplicacion");    
+            
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        
+        int numero7 = scanner.nextInt();
+        
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero8 = scanner.nextInt();
+        
+        int multi = numero7 * numero8;
+            System.out.println("la resta de " + numero7 + " / " + numero8 + " es: " + multi);
+            
+        scanner.close();
+        scanner.close();
     }
 }
